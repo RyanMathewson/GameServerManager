381fc0d240348c76170887fa42e4d83ce9944209
Ryan Mathewson <RyanMathewson@users.noreply.github.com>
2025-06-28 22:16:08 -0400

    Fixed naming in install script

--- GameServerManagerService/install_service.bat
+++ GameServerManagerService/install_service.bat
@@ -1,12 +1,12 @@
 @echo off
-REM Install the SteamManagerService as a Windows service
-set SERVICE_NAME=SteamManagerService
-set EXE_PATH=%~dp0SteamManagerService.exe
+REM Install the GameServerManagerService as a Windows service
+set SERVICE_NAME=GameServerManagerService
+set EXE_PATH=%~dp0GameServerManagerService.exe
 
 sc stop %SERVICE_NAME%
 sc delete %SERVICE_NAME%
 sc create %SERVICE_NAME% binPath= "%EXE_PATH%" start= auto
-sc description %SERVICE_NAME% "Steam Manager Service"
+sc description %SERVICE_NAME% "Game Server Manager Service"
 sc start %SERVICE_NAME%
 
 echo Service installation complete.
